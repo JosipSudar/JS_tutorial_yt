8e6067a0b87ebf7114d8ab9fdaf7862f013740ab
JosipSudar <josip.sudar@gmail.com>
2024-01-10 18:57:40 +0100

    first

---
+++ 1 - JavaScript tutorial for beginners/index.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+console.log("I like pizza!");
+
+window.alert("This is an alert!");
+window.alert("I like pizza!");
+
+document.getElementById("myh1").textContent = "hello";
+document.getElementById("myp").textContent = "I like pizza!";
+
+//This is a comment
+
+/*This is a comment*/
