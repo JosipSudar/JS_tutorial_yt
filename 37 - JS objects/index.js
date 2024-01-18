974c312a489e300f5d782dcb28618a1b5f4a6a24
JosipSudar <josip.sudar@gmail.com>
2024-01-18 10:12:13 +0100

    18/01/2024

---
+++ 37 - JS objects/index.js
@@ -0,0 +1,28 @@
+// object = A collection of related properties and/or methods
+//          Can represent real world objects (people, products, places)
+//          object = {key: value, function()}
+
+const person1 = {
+  firstName: "Spongebob",
+  lastName: "Squarepants",
+  age: 30,
+  employed: true,
+  sayHello: function () {
+    console.log("Hi! I am Spongebob!");
+  },
+  eat: function () {
+    console.log("I am eating a Krabby Patty");
+  },
+};
+
+const person2 = {
+  firstName: "Patrick",
+  lastName: "Star",
+  age: 42,
+  employed: false,
+  sayHello: () => console.log("Hi! I'm Patrick..."),
+  eat: () => console.log("I am eating roast beef, chicken and pizza"),
+};
+
+person1.eat();
+person2.eat();
